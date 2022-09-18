438fc5306938dfc49bb251dfae2fa9d80152ddfd
Maria Khudyakova <mariahydakova71@gmail.com>
2022-09-19 00:58:22 +0500

    Поправлен код в main.js с помощью nmp

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
+// eslint-disable-next-line no-shadow
 function getRandomIntInclusive(start, finish) {
-    start = Math.ceil(start);
-    finish = Math.floor(finish);
-    return Math.floor(Math.random() * (finish - start + 1)) + start;
+  start = Math.ceil(start);
+  finish = Math.floor(finish);
+  return Math.floor(Math.random() * (finish - start + 1)) + start;
 } //Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
+// eslint-disable-next-line no-shadow
 function checkMaxLength(str, maxLen) {
-    return str.length <= maxLen;
+  return str.length <= maxLen;
 }
 
 //Код ниже предназначен по большей части для ручной проверки
@@ -14,5 +16,7 @@ let finish;
 let str;
 let maxLen;
 
+// eslint-disable-next-line no-console
 console.log(getRandomIntInclusive(start, finish));
+// eslint-disable-next-line no-console
 console.log(checkMaxLength(str, maxLen));
