87f664a950eea0d8154d5f9a3b248cac5e25590c
Maria Khudyakova <mariahydakova71@gmail.com>
2022-09-19 00:12:46 +0500

    Вводит функции получения случайного числа и проверки макс длины строки в main.js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function getRandomIntInclusive(start, finish) {
+    start = Math.ceil(start);
+    finish = Math.floor(finish);
+    return Math.floor(Math.random() * (finish - start + 1)) + start;
+} //Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+
+function checkMaxLength(str, maxLen) {
+    return str.length <= maxLen;
+}
+
+//Код ниже предназначен по большей части для ручной проверки
+let start;
+let finish;
+let str;
+let maxLen;
+
+console.log(getRandomIntInclusive(start, finish));
+console.log(checkMaxLength(str, maxLen));
