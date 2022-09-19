c483f8f7a6e8701ef4ffdc8c482fea8f237385c5
Maria Khudyakova <mariahydakova71@gmail.com>
2022-09-19 10:40:29 +0500

    Задание: Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
-// eslint-disable-next-line no-shadow
+/**
+ * Функция, возвращающая случайное целое число из переданного диапазона включительно.
+ * Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ * @param {number} start - первое число диапазона
+ * @param {number} finish - последнее число диапазона
+ * @returns {number} - случайное число из заданного диапазона
+ */
 function getRandomIntInclusive(start, finish) {
+  if (start < 0 || finish >= start) {
+    throw {name : 'Invalid arguments', message : 'You pussed invalid arguments'};
+  }
+
   start = Math.ceil(start);
   finish = Math.floor(finish);
   return Math.floor(Math.random() * (finish - start + 1)) + start;
-} //Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+}
 
-// eslint-disable-next-line no-shadow
+getRandomIntInclusive(-1, 20);
+
+/**
+ * Функция для проверки максимальной длины строки.
+ * @param {object} str - проверяемая строка
+ * @param {number} maxLen - максимальная длина строки
+ * @returns {boolean} - true - строка подходит по длине, false - не подходит
+ */
 function checkMaxLength(str, maxLen) {
+  str = String(str);
   return str.length <= maxLen;
 }
 
-//Код ниже предназначен по большей части для ручной проверки
-let start;
-let finish;
-let str;
-let maxLen;
-
-// eslint-disable-next-line no-console
-console.log(getRandomIntInclusive(start, finish));
-// eslint-disable-next-line no-console
-console.log(checkMaxLength(str, maxLen));
+checkMaxLength('qwertyuiopasdfghjvb', 140);
